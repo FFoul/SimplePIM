f4d1d5475486657d445f2159b394e44584daba7e
lihengluo <wailiheng@gmail.com>
2024-04-11 21:16:52 +0800

    fea:the param.h can be right with mixture of define and const.

--- benchmarks/kmeans/Param.h
+++ benchmarks/kmeans/Param.h
@@ -5,10 +5,15 @@
 #include <stdint.h>
 uint32_t print_info = 0;
 typedef int32_t T; 
-const uint32_t dpu_number = 5; // 2432
-const uint32_t k = 10;
+const uint32_t dpu_number = 5; // 243
+
+#define K 10
+const uint32_t k = K;
+const uint32_t num_elements = 1000 * K;
+
+// const uint32_t k = 10;
 const uint32_t dim = 10;
-#define num_elements 1000*dpu_number
+// const uint32_t num_elements  = 1000 * 5;
 const uint32_t iter = 1;
 
 #endif
